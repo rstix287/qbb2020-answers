94d4b6df5076aca86b99b2f2adf0a3a511a16d56
Robyn Stix <rstix1@jh.edu>
2020-09-01 14:20:18 -0400

    added comments

--- day2-lunch/day2-lunch-1.py
+++ day2-lunch/day2-lunch-1.py
@@ -2,14 +2,16 @@
 
 import numpy as np
 
-#read in file and lines
+#read in file and lines of file from fly.txt
 map_file = []
 fly = open('fly.txt', 'r')
+# checks if 'DROME' and 'FBgn' are in the line,
+# if so, it appends the fly id and the uniprot id (tab separated) to array
 for line in fly:
     if 'DROME' in line and 'FBgn' in line:
         data = line.split()
         map_file.append(data[-1] + '\t' + data[-2])
 fly.close()
-
+# writes out array to file
 np.savetxt('day2-lunch-1.out', map_file, fmt="%s")
 
--- day2-lunch/day2-lunch-2.py
+++ day2-lunch/day2-lunch-2.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python3
+
+# function to replace rseq data with uniprot ID 
 def change_ID(map_id, c_tab, replace = 'no'):
-    #read in file and lines
+    #read in map_id file and lines and creates dictionary
     map_id_f = open(map_id, 'r')
     map_id_d = {}
+    #adds each fly id as key and uniprot id as the value to dictionary
     for line in map_id_f:
         fly_id_up = line.split()
         map_id_d.setdefault(fly_id_up[0], fly_id_up[1])
     map_id_f.close()
 
+    #reads c_tab file
     new_rseq = []
     rseq = open(c_tab, 'r')
     for line in rseq:
         rseq_split = line.split()
+        # if header skips
         if rseq_split[0] == 't_id':
             continue
+        # if specified not to replace if fly id isnt found skips
         elif map_id_d.get(rseq_split[8], -1) == -1 and replace == 'no':
             continue
+        # if specified to replace if fly id not found, replaces with 'NA'
         elif map_id_d.get(rseq_split[8], -1) == -1 and replace == 'yes':
             rseq_split[8] = 'NA'
             new_rseq.append('\t'.join(map(str, rseq_split)))
+        # inputs corresponding UniProt id of fly id in rseq data
         else:
             rseq_split[8] = map_id_d.get(rseq_split[8])
             new_rseq.append('\t'.join(map(str, rseq_split)))
     rseq.close()
-
+    # prints rseq data
     np.savetxt('day2-lunch-2.out', new_rseq, fmt="%s")
 
 
 if __name__== "__main__":
     import sys
     import numpy as np
-
-    change_ID(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(sys.argv) ==2:
+        change_ID(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        change_ID(sys.argv[1], sys.argv[2])
